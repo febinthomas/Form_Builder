ef3dc172d937b24f1fe5ba6f092166bb7af5c440
Febin <febinthomas@outlook.com>
2025-08-14 14:11:48 +0200

    Pint changes PSR12

--- app/Http/Requests/StoreFormRequest.php
+++ app/Http/Requests/StoreFormRequest.php
@@ -32,7 +32,7 @@ class StoreFormRequest extends FormRequest
             'custom_form_fields.*.form_field_type_id' => [
                 'required',
                 'integer',
-                Rule::exists((new FormFieldType)->getTable(), 'id'),
+                Rule::exists((new FormFieldType())->getTable(), 'id'),
             ],
             'custom_form_fields.*.values.*.option' => ['string'],
             'custom_form_fields.*.values.*.label' => ['string'],
